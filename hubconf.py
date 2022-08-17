9d43d910bb56970455614a9c138a40ece9f329ca
Asthestarsfalll <1186454801@qq.com>
2022-08-17 13:22:34 +0800

    add hubconf

---
+++ hubconf.py
@@ -0,0 +1,6 @@
+from image_classification.models.uniformer import (UniFormer,
+                                                   uniformer_small,
+                                                   uniformer_small_plus,
+                                                   uniformer_small_plus_dim64,
+                                                   uniformer_base,
+                                                   uniformer_base_ls)
